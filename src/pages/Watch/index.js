890f4ff5c3df88f9d628df60de223b2fa6fa624d
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2024-07-10 16:23:35 -0400

    sera

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,7 +1,7 @@
 import Header from '../../components/Header';
 import Container from '../../components/Container';
 import Footer from '../../components/Footer';
-import styles from './Watch.molule.css';
+import styles from './Watch.module.css';
 import { useParams } from 'react-router-dom';
 import videos from "../../json/videos.json";
 
@@ -18,7 +18,7 @@ function Watch() {
                 <section className={styles.watch}>
                         <iframe width="855"
                         height="480"
-                        src={`https://www.youtube.com/embed/${video.id}?si=-h2jWW-CdcO2IO3U`}
+                        src={`https://www.youtube.com/embed/${video.id}`}
                         title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
