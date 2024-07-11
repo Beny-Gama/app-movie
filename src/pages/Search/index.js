78c459ea429003ca5e67312220efddad42431e80
Bernardo Gama <76955720+Beny-Gama@users.noreply.github.com>
2024-07-11 00:25:10 -0400

    Criar Componente VideoList e Página Pesquisar  Professor Edson Maia 15,3 mil inscritos  Inscrever-se

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,7 +1,9 @@
 import style from './Search.module.css';
-import Footer from '../../components/Footer';
+import Footer from '../../components/Footer'; 
 import Header from '../../components/Header';
 import Container from '../../components/Container';
+import VideoList from '../../components/VideoList';
+import videos from '../../json/videos.json';
 
 function Search() {
     return (
@@ -10,6 +12,8 @@ function Search() {
         <Container>
         <section className={style.search}>
             <h2>Perguisar</h2>
+            
+            <VideoList videos={videos}/>
         </section>
         </Container>
         <Footer />
